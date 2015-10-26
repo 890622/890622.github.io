6f623c4db59d46255689a9dca83264b5404d8c90
Joachim Wernersson <joachim.wernersson@gmail.com>
2015-10-26 11:46:51 +0100

    Maybe I merged the two and pulled now?

--- js/jquery.js
+++ js/jquery.js
@@ -3,11 +3,8 @@ $("#nav_button").click(function() {
 	$("#nav_button").hide();
 });
 
-<<<<<<< HEAD
 $(".opacity_filter").click(function() {
 	$(".opacity_filter").remove();
-=======
+
 $("#black_opacity_filter").click(function() {
 	$("#black_opacity_filter").hide();
->>>>>>> 57fdc3a12da0444e1fd4994a052f5bf65e69c600
-});
