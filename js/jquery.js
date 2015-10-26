fc9741eca4feeaf9f10a15097025e6cc00c6804c
Joachim Wernersson <joachim.wernersson@gmail.com>
2015-10-26 11:51:58 +0100

    Commented out jquery

--- js/jquery.js
+++ js/jquery.js
@@ -3,8 +3,10 @@ $("#nav_button").click(function() {
 	$("#nav_button").hide();
 });
 
-$(".opacity_filter").click(function() {
-	$(".opacity_filter").remove();
+// $(".opacity_filter").click(function() {
+// 	$(".opacity_filter").remove();
+// });
 
-$("#black_opacity_filter").click(function() {
-	$("#black_opacity_filter").hide();
+// $("#black_opacity_filter").click(function() {
+// 	$("#black_opacity_filter").hide();
+// });
